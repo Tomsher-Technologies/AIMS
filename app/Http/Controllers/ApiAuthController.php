4c8224b3ce053fac92be18c12da4027f98d27845
Jishap-Tomsher <jishap.tomsher@gmail.com>
2023-07-25 15:41:15 +0400

    API changes

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -200,21 +200,7 @@ class ApiAuthController extends Controller
         ]);
     }
 
-    // Get all countries
-    public function getCountries(){
-        $countries = Countries::select('id','name')->orderBy('name','ASC')->get();
-        return response()->json([ 'status' => true, 'message' => 'Success', 'data' => $countries]);
-    }
-
-    // Get all states and states based on the passed country_id 
-    public function getCountryStates(Request $request){
-        $query = States::select('*');
-        if(isset($request->country_id)){
-            $query->where('country_id', $request->country_id);
-        }
-        $states = $query->orderBy('name','ASC')->get();
-        return response()->json([ 'status' => true, 'message' => 'Success', 'data' => $states]);
-    }
+    
 
     // Get all courses list with course divisions
     public function getAllCourses() {
---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,24 @@
+    <div class="menu">
+        <div class="main-menu">
+            <div class="scroll">
+                <ul class="list-unstyled">
+                    <li class="active">
+                        <a href="dashboard.html">
+                            <i class="iconsminds-shop-4"></i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="certificates.html">
+                            <i class="iconsminds-digital-drawing"></i> Certificates
+                        </a>
+                    </li>
+                    <li>
+                        <a href="admin_roles.html">
+                            <i class="iconsminds-air-balloon-1"></i> Admin
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
